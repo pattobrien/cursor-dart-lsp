90beba94a620a8caecc5bbee4158733af1baa550
pattobrien <12474318+pattobrien@users.noreply.github.com>
2025-03-31 19:44:30 -0400

    first commit

---
+++ bin/cursor_dart_lsp.dart
@@ -0,0 +1,5 @@
+import 'package:cursor_dart_lsp/cursor_dart_lsp.dart' as cursor_dart_lsp;
+
+void main(List<String> arguments) {
+  print('Hello world: ${cursor_dart_lsp.calculate()}!');
+}
---
+++ test/cursor_dart_lsp_test.dart
@@ -0,0 +1,8 @@
+import 'package:cursor_dart_lsp/cursor_dart_lsp.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
